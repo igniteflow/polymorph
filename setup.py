e781d665d958cf1ab83e8cd83bf0c105244a60c9
Phil Tysoe <igniteflow@users.noreply.github.com>
2017-03-07 12:19:23 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='polymorph',
+      version='0.1',
+      description='Python tooling to tranform data',
+      url='https://github.com/igniteflow/polymorph',
+      author='Phil Tysoe',
+      author_email='philtysoe@gmail.com',
+      license='MIT',
+      packages=['polymorph'],
+      zip_safe=False
+)
